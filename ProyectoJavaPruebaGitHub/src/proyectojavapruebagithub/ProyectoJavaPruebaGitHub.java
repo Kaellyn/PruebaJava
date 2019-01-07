932a12f3d5e98b551b22474f9e9c5bb4b3ccce85
Henry Aguilar <henrysagq@gmail.com>
2019-01-07 10:15:35 -0500

    Cambie Nombre
    Cambio Jonathan por Henry

--- ProyectoJavaPruebaGitHub/src/proyectojavapruebagithub/ProyectoJavaPruebaGitHub.java
+++ ProyectoJavaPruebaGitHub/src/proyectojavapruebagithub/ProyectoJavaPruebaGitHub.java
@@ -17,7 +17,7 @@ public class ProyectoJavaPruebaGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Gente");
-		        System.out.println("Hola me llamo jonathan");
+		        System.out.println("Hola me llamo Henry");
 		        System.out.println("como estan");
 
     }
